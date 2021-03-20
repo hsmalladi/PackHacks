d15ca4b5ba78d0701fe0d2bcdcb54f028b04b424
mkwak <minjunkwak@gmail.com>
2021-03-20 13:10:20 -0400

    loginpage

--- my-app/src/pages/HomePage.js
+++ my-app/src/pages/HomePage.js
@@ -1,12 +1,9 @@
 import React from "react";
-import LoginPage from "./LoginPage";
-import { Link } from "react-router-dom";
 
 const HomePage = () => {
     return (
         <div>
             <div>Hello World!</div>
-            <Link to="./login" className="btn btn-primary">hello</Link>
         </div>
     );
 }
