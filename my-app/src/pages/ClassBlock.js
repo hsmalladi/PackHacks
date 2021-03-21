ec8f3fe6f6d7c1dd665957111e504695c8139dff
Harry Wang <harry.s.wang@duke.edu>
2021-03-20 21:54:03 -0400

    created class and classblock pages

---
+++ my-app/src/pages/ClassBlock.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function ClassBlock(props) {
+    const peopleList = [
+        {
+            name: "Bryan Pan",
+            insta: "https://www.instagram.com/_bryanpan_/",
+            face: "",
+            email: "",
+        }, 
+        {
+            name:"Minjun Kwak",
+            insta:""
+        }, 
+        {
+            name: "Harry Wang",
+            insta: "https://www.instagram.com/harr.yw/",
+            face: "https://www.facebook.com/harry.wang.5454",
+            email: "hsw20@duke.edu"
+        }, 
+        {"Shawn Lee"}, 
+        {"Nirvan Silswal"}, 
+        {"Bald Ma"}
+    ];
+    return(
+        <div>
+            <h2> {props.courseName} </h2>
+            <ul>
+                <li>{peopleList[Math.floor(Math.random() * 6)]}</li>
+                <li>{peopleList[Math.floor(Math.random() * 6)]}</li>
+                <li>{peopleList[Math.floor(Math.random() * 6)]}</li>
+            </ul>
+        </div>
+    );
+}
+
+export default ClassBlock;
